54da12d471b30c90ade7161df637e717974e77dc
deoresulakshana@gmail.com <36725147+sulakshanadeore@users.noreply.github.com>
2022-09-12 10:08:58 +0530

    Add project files.

---
+++ MyMathsLibrary/SimpleCalculations.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMathsLibrary
+{
+    public class SimpleCalculations
+    {
+        public int add(int i, int j)
+        {
+            return i + j;
+        }
+
+
+    }
+}
---
+++ ReflectionDemo/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+namespace ReflectionDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"D:\Sulakshana\MphasisAugust\ReflectionDemo\MyMathsLibrary\bin\Debug\MyMathsLibrary.dll";
+
+            Assembly asm=Assembly.LoadFrom(path);
+            //  Assembly asm1=Assembly.GetAssembly(typeof(path));
+            Type[] types=asm.GetTypes();
+            foreach (var item in types)
+            {
+                Console.WriteLine("name= " + item.Name);//classname
+                Console.WriteLine("Full Name= "  + item.FullName);//assemblyname/namespacename and class
+                Console.WriteLine("IS Class=" + item.IsClass);
+                Console.WriteLine("IS Absatract="  +item.IsAbstract);
+                Console.WriteLine("Whether public " + item.IsPublic);
+                
+
+                Console.WriteLine("-----------------");
+                MemberInfo[] minfo=item.GetMembers();
+
+                foreach (var minfoitem in minfo)
+                {
+
+                    Console.WriteLine("Member Info Name= " + minfoitem.Name);
+                    
+                    Console.WriteLine("Declaring type=" + minfoitem.DeclaringType);
+                }
+
+                Console.WriteLine("---------------------");
+                MethodInfo[] methodInfos=item.GetMethods();
+                foreach (var methodInfosItem in methodInfos)
+              {
+
+                    Console.WriteLine("name" + methodInfosItem.Name);
+                    Console.WriteLine("IS PUBLIC" + methodInfosItem.IsPublic);
+                    Console.WriteLine("IS STATIC =" + methodInfosItem.IsStatic);
+
+                    ParameterInfo[] pinfo=methodInfosItem.GetParameters();
+                    Console.WriteLine("---Add method Parameters----");
+                    if (methodInfosItem.Name=="add")
+                    {
+                        foreach (var par in pinfo)
+                        {
+                            Console.WriteLine(par.Name);
+                            Console.WriteLine(par.Position);
+                            Console.WriteLine(par.ParameterType);
+
+
+                        }
+                    }
+                    
+
+                }
+
+
+
+                
+            }
+
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
